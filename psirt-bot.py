205d652072063d4ea47469cbd51263187c3c21b8
Aaron Davis <aaron.e.davis@gmail.com>
2022-07-13 16:58:03 -0600

    configured dynamic summary info

--- psirt-bot.py
+++ psirt-bot.py
@@ -161,7 +161,128 @@ def card_build(cve_count, cve_recent, x):
     return wa_sum_msg
 
 
+def psirt_otoken(psirt_f_grant, psirt_f_client_id, psirt_f_client_secret):
+    """This function creates the PSIRT OAuth token
+
+    Args:
+        grant (str): Token grant type
+            (https://raw.githubusercontent.com/api-at-cisco/Images/master/Token_Access.pdf)
+        client_id (str): API username
+        client_secret (str): API password
+
+    Returns:
+        access_token (str): Access token
+        token_type (str): Token type ("Bearer")
+        token_dies (time): When token expires
+    """
+
+    otoken_url = (
+        f"https://cloudsso.cisco.com/as/token.oauth2?grant_type={psirt_f_grant}"
+        f"&client_id={psirt_f_client_id}&client_secret={psirt_f_client_secret}"
+    )
+
+    try:
+        otoken_response = requests.request("POST", otoken_url)
+        otoken_response.raise_for_status()
+    except requests.HTTPError:
+        otoken_status = otoken_response.status_code
+        if otoken_status == 401:
+            logging.error("Invalid API key.")
+        elif otoken_status == 404:
+            logging.error("Invalid input.")
+        elif otoken_status in (429, 443):
+            logging.error("API calls per minute exceeded.")
+        elif otoken_status == 400:
+            logging.error("API bad request.")
+        sys.exit(1)
+
+    otoken_data = otoken_response.json()
+    otoken_access_token = otoken_data["access_token"]
+    otoken_token_type = otoken_data["token_type"]
+    otoken_token_expires = otoken_data["expires_in"]
+    otoken_token_dies = time() + (otoken_token_expires - 120)
+
+    return (otoken_access_token, otoken_token_type, otoken_token_dies)
+
+
+def recent_update(verify_cve_date):
+    """Determines if CVE entry has been updated in last 7 days
+
+    Args:
+        verify_cve_date (string): yyyy-mm-ddThh:mm:ss
+
+    Returns:
+        bool: True if entry has been updated in last 7 days
+    """
+    t_index = verify_cve_date.index("T")
+    stripped_date = verify_cve_date[:t_index:]
+    split_date = tuple(stripped_date.split("-"))
+    new_date = date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
+    seven_days = date.today() - timedelta(days=7)
+    recent = seven_days < new_date
+    return recent
+
+
 logging.info("------------------------------------------------------")
+
+# Get PSIRT OAUTH
+otoken_token, otoken_type, otoken_expiry = psirt_otoken(
+    psirt_grant, psirt_client_id, psirt_client_secret
+)
+
+
+# Get latest PSIRT summary data
+
+TODAY = date.today()
+TODAY_STR = str(TODAY)
+DELTA = timedelta(days=90)
+NINETY_DAYS = TODAY - DELTA
+NINETY_DAYS_STR = str(NINETY_DAYS)
+
+psirt_url = (
+    f"https://api.cisco.com/security/advisories/all/firstpublished"
+    f"?startDate={NINETY_DAYS_STR}&endDate={TODAY_STR}"
+)
+
+psirt_token = f"Bearer {otoken_token}"
+psirt_headers = {"Authorization": psirt_token}
+
+try:
+    psirt_response = requests.request("GET", psirt_url, headers=psirt_headers)
+    psirt_response.raise_for_status()
+except requests.HTTPError:
+    status = psirt_response.status_code
+    if status in (401, 403):
+        logging.error("Invalid PSIRT API key.")
+    elif status == 404:
+        logging.error("Invalid PSIRT request input.")
+    elif status in (429, 443):
+        logging.error("PSIRT API calls per minute exceeded.")
+    sys.exit(1)
+
+psirt_json_response = json.loads(psirt_response.text)
+
+# End of PSIRT request
+
+# Get number of CVE's > 90-days and those updated in last 7-days
+
+cve_entries = psirt_json_response["advisories"]
+
+CVE_ENTRY_COUNT = 1
+CVE_UPDATED_ENTRIES = 0
+
+for entry in cve_entries:
+    last_updated = entry["lastUpdated"]
+    fresh_update = recent_update(last_updated)
+    if fresh_update is True:
+        CVE_UPDATED_ENTRIES += 1
+
+    CVE_ENTRY_COUNT += 1
+
+logging.info("Total number of CVE entries: %s", CVE_ENTRY_COUNT)
+logging.info("Number of updated CVE entries: %s", CVE_UPDATED_ENTRIES)
+
+# End of conversion
 
 new_record = collection.find({"response": {"$exists": False}})
 num_records = collection.count_documents({"response": {"$exists": False}})
@@ -220,10 +341,8 @@ for _ in range(num_records):
 for _ in valid_object_id:
     # Get room ID & Report Type
     reply_room_Id, report_type = get_rm_rpt(_)
-    cve_cnt = 95  # TODO: Build this piece out
-    cve_updates = 10
     report_days = 7
-    wa_card_attach = card_build(cve_cnt, cve_updates, report_days)
+    wa_card_attach = card_build(CVE_ENTRY_COUNT, CVE_UPDATED_ENTRIES, report_days)
     attach_url = (
         f"https://docs.google.com/spreadsheets/d/e/{gsheet_doc_link}/pub?output="
     )
